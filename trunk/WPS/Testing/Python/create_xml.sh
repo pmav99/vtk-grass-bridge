fcd076561386fc760fe005da4a42aa66c41d8616
soerengebbert <soerengebbert@195681ba-a6f5-11de-b195-ab392a2a73f5>
2010-05-24 22:38:19 +0000

    Implemented ZOO WPS support. Refractoring of GrassModuleStarter.

--- trunk/WPS/Testing/Python/create_xml.sh
+++ trunk/WPS/Testing/Python/create_xml.sh
@@ -5,7 +5,7 @@ python GrassAddons/r.mult --wps-process-description > r.mult.xml
 r.contour --wps-process-description > r.contour.xml
 r.watershed --wps-process-description > r.watershed.xml
 r.univar --wps-process-description > r.univar.xml
-r.neighbors --wps-process-description > r.neighbours.xml
+r.neighbors --wps-process-description > r.neighbors.xml
 v.buffer --wps-process-description > v.buffer.xml
 v.voronoi --wps-process-description > v.voronoi.xml
 v.to.rast --wps-process-description > v.to.rast.xml
