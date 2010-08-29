e40d602de3d553df7851d6e9d8541a3b9e71cdf8
soerengebbert <soerengebbert@195681ba-a6f5-11de-b195-ab392a2a73f5>
2010-08-29 23:37:31 +0000

    Corrected license

--- trunk/Filtering/vtkGRASSBridgeFilteringWin32Header.h
+++ trunk/Filtering/vtkGRASSBridgeFilteringWin32Header.h
@@ -1,14 +1,16 @@
-/*=========================================================================
- FPla library
- Authors: Soeren Gebbert
- Created: Aug. 2009
-
-
- =========================================================================*/
-/// \brief manage Windows system differences
-/// 
-/// The Header captures some system differences between Unix
-/// and Windows operating systems. 
+/*
+ * Program: vtkGRASSBridge
+ * COPYRIGHT: (C) 2010 by Soeren Gebbert, soerengebbert@googlemail.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+*/
 
 #ifndef __vtkGRASSBridgeFilteringWin32Header_h
 #define __vtkGRASSBridgeFilteringWin32Header_h
--- trunk/Graphics/vtkGRASSBridgeGraphicsWin32Header.h
+++ trunk/Graphics/vtkGRASSBridgeGraphicsWin32Header.h
@@ -1,14 +1,17 @@
-/*=========================================================================
- VTK GRASS Bridge
- Authors: Soeren Gebbert
- Created: Aug. 2007
+/*
+ * Program: vtkGRASSBridge
+ * COPYRIGHT: (C) 2009 by Soeren Gebbert, soerengebbert@googlemail.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+*/
 
-
- =========================================================================*/
-/// \brief manage Windows system differences
-/// 
-/// The Header captures some system differences between Unix
-/// and Windows operating systems. 
 
 #ifndef __vtkGRASSBridgeGraphicsWin32Header_h
 #define __vtkGRASSBridgeGraphicsWin32Header_h
